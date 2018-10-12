06ed311ba9a65ea9717a0c587f30f4fe3b2f5554
anjali-12 <43993494+anjali-12@users.noreply.github.com>
2018-10-13 01:02:13 +0530

    Add files via upload

---
+++ guvi6.py
@@ -0,0 +1,13 @@
+n=int(input())
+if n%4==0:
+    if n%100==0:
+        if n%400:
+            print("yes")
+        else:
+            print("no")
+    else:
+        print("yes")
+else:
+    print("no")
+
+        
