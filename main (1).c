ac128e1ba8015b12c76b18dafc38efdd6635c4e0
anjali-12 <43993494+anjali-12@users.noreply.github.com>
2018-10-15 21:53:26 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a,b;
+    int result;
+    printf("enter values");
+   scanf("%d%d",&a,&b);
+   result=pow(a,b);
+   printf("%d",result);
+}
+
