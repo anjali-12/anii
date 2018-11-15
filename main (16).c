cd890774b6b60032f3ae0fee6804a97eca2e82bd
anjali-12 <43993494+anjali-12@users.noreply.github.com>
2018-11-15 19:14:50 +0530

    Add files via upload

---
+++ main (16).c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a[10],n,i;
+    
+    printf("enter the size of array");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {scanf("%d",&a[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    {
+        printf("%d %d\n",a[i],i);
+    }
+}
+
