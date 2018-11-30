5e5e649e48749f621955db8ff635f875f58f6c00
anjali-12 <43993494+anjali-12@users.noreply.github.com>
2018-12-01 00:54:39 +0530

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,16 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+n1=int(input())
+n2=int(input())
+n=n1+n2
+if(n%2==0):
+    print("even")
+    
+else:
+    print("odd")
