2a2ac37ddf54f6f01af7bd1f43e01e2f988c5b77
anjali-12 <43993494+anjali-12@users.noreply.github.com>
2018-12-01 00:45:12 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,18 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+l=[]
+for i in range(10):
+    n=input()
+    l.append(n);
+    
+k=sorted(l)
+print(k)
+min=l[0]
+print(min)
+    
